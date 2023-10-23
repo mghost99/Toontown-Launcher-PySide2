38e9fe3817ca1c18577dce7d5bf0729e8bef166e
mghost99 <10858729+mghost99@users.noreply.github.com>
2023-10-23 01:51:41 -0500

    game_launch: slimmed down
    Linux troubleshooting but issue persists.
    Game reopens after second closure, multiplying every game launch when launcher is left open.
    We will figure this out.

--- src/util/game_launch.py
+++ src/util/game_launch.py
@@ -1,7 +1,8 @@
 import os
 import platform
 import subprocess
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
+import time
+from PyQt5.QtCore import QObject, pyqtSignal
 
 class GameLauncher(QObject):
     game_exit_signal = pyqtSignal(int, str)
@@ -11,72 +12,37 @@ class GameLauncher(QObject):
         super().__init__()
         self.urls = urls
         self.main_window = main_window
-        self.launcher_thread = None
-        self.launcher_worker = None
 
     def launch_game(self):
-        self.launcher_thread = QThread()
-        self.launcher_worker = LauncherWorker(self.urls)
-        self.launcher_worker.moveToThread(self.launcher_thread)
-        self.launcher_worker.launch_signal.connect(self.launch)
-        self.launcher_thread.started.connect(self.iconify_launcher)
-        self.launcher_thread.started.connect(self.launcher_worker.run)
-        self.launcher_worker.finished.connect(self.launcher_thread.quit)
-        self.launcher_worker.finished.connect(self.launcher_worker.deleteLater)
-        self.launcher_thread.finished.connect(self.launcher_thread.deleteLater)
-        self.launcher_thread.start()
+        cmd = self.prepare_command()
+        if cmd:
+            self.main_window.showMinimized()
+            game_process = subprocess.Popen(cmd)
+            self.monitor_game_exit(game_process)
 
-    def iconify_launcher(self):
-        self.main_window.showMinimized()
-
-    def launch(self, cmd):
-        try:
-            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            self.monitor_game_exit(process)
-        except Exception as e:
-            error_message = f"An error has occurred launching Toontown: {e}"
-            self.game_exit_signal.emit(-1, error_message)
-
-    def monitor_game_exit(self, process):
-        while True:
-            retcode = process.poll()
-            if retcode is not None:
-                stdout, stderr = process.communicate()
-                if retcode != 0:
-                    error_message = stderr.decode('utf-8')
-                    self.game_exit_signal.emit(retcode, error_message)
-                else:
-                    self.game_exit_signal.emit(retcode, "Thanks for playing Toontown!")
-                self.game_closed_signal.emit()
-                break
-
-
-class LauncherWorker(QObject):
-    launch_signal = pyqtSignal(list)
-    finished = pyqtSignal()
-
-    def __init__(self, urls):
-        super().__init__()
-        self.urls = urls
-
-    def run(self):
+    def prepare_command(self):
         system = platform.system()
         cwd = os.getcwd()
-
         tt_exe = os.path.join(cwd, 'Toontown.exe')
         os.environ['DOWNLOAD_SERVER'] = self.urls['PATCHER_BASE_URL_HEAVY_LIFTING']
         os.environ['PANDA_DOWNLOAD_URL'] = self.urls['PANDA_DOWNLOAD_URL']
         os.environ['GAME_WHITELIST_URL'] = self.urls['GAME_WHITELIST_URL']
         os.environ['ACCOUNT_SERVER'] = self.urls['ACCOUNT_SERVER']
 
-        cmd = None
         if system == "Linux":
-            cmd = ["wine", tt_exe]
+            return ["wine", tt_exe]
         elif system == "Windows":
-            cmd = tt_exe
+            return [tt_exe]
         elif system == "Darwin":
-            pass  # TODO
+            return None  # TODO: Add command for macOS
 
-        if cmd:
-            self.launch_signal.emit(cmd)
-        self.finished.emit()
+    def monitor_game_exit(self, process):
+        while process.poll() is None:
+            time.sleep(1)
+        retcode = process.returncode
+        if retcode != 0:
+            error_message = "Game exited with an error."
+            self.game_exit_signal.emit(retcode, error_message)
+        else:
+            self.game_exit_signal.emit(retcode, "Thanks for playing Toontown!")
+        self.game_closed_signal.emit()
