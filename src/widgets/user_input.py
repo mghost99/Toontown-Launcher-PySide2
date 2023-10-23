60d9f93b91e295d7b54c5286a88248ac10165661
mghost99 <10858729+mghost99@users.noreply.github.com>
2023-10-22 23:24:32 -0500

    widgets: remove unused modules

--- src/widgets/user_input.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QLineEdit
-
-class UserInput(QLineEdit):
-    def __init__(self, parent=None):
-        super(UserInput, self).__init__(parent)
-        
-        # Setting the size and position of the text box
-        self.setGeometry(499, 172, 140, 18)
